6b2f068c017e20dd29a72acd3d5a94c0bd177360
Hyunjun Yoon <yhj940719@gmail.com>
2021-08-27 17:27:35 -0400

    Sign Up Controller added

--- AspNetBlog/AspNetBlog.MVC6/Controllers/AccountController.cs
+++ AspNetBlog/AspNetBlog.MVC6/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetBlog.MVC6.DataContext;
+using AspNetBlog.MVC6.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,8 +18,23 @@ namespace AspNetBlog.MVC6.Controllers
         {
             return View();
         }
-        public IActionResult Register()
+        public IActionResult SignUp()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(User  model)
+        {
+            if(ModelState.IsValid)
+            {              
+                using (var db = new AspNetDbContext())
+                {
+                    db.Users.Add(model);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 
